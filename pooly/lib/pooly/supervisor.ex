5fd41b97b10f992912c178d49f2afaba0b8b5050
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2022-09-19 01:20:42 +0200

    Pooly: v1: Add some warning about unexpected behaviour

--- pooly/lib/pooly/supervisor.ex
+++ pooly/lib/pooly/supervisor.ex
@@ -11,6 +11,21 @@ defmodule Pooly.Supervisor do
     # high-level supervisor it receives by config here.
     # So in the end both processes are supervised by
     # this supervisor as siblings.
+    # ----------------------------------------------------
+    # Warning This is what I thought it would happen
+    # **BUT**
+    # despite the fact that Server uses the this Supervisor
+    # to start WorkerSupervisor
+    # **AND**
+    # that this supervisor
+    # would have the `:one_for_all strategy`
+    # Seems that dynamically started childs do NOT obey the
+    # same strategy
+    # **BECAUSE**
+    # when I have killed WorkerSupervisor nothing happens
+    # but if I kill Server everything (Server and WorkerSupervisor)
+    # is restarted.
+    # ----------------------------------------------------
     children = [
       worker(Pooly.Server, [self(), pool_config])
     ]
