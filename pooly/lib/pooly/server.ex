2497285acf62e6031faf32dc332e91c85dd52020
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2022-09-18 23:56:49 +0200

    Pooly: v1: Add status info command

--- pooly/lib/pooly/server.ex
+++ pooly/lib/pooly/server.ex
@@ -43,6 +43,10 @@ defmodule Pooly.Server do
     GenServer.cast(__MODULE__, {:checkin, worker_pid})
   end
 
+  def status do
+    GenServer.call(__MODULE__, :status)
+  end
+
   ############
   # CallBacks
   ############
@@ -112,6 +116,10 @@ defmodule Pooly.Server do
       [] ->
         {:reply, :noproc, state}
     end
+  end
+
+  def handle_call(:status, _from, %{workers: workers, monitors: monitors} = state) do
+    {:reply, {length(workers), :ets.info(monitors, :size)}, state}
   end
 
   def handle_cast({:checkin, worker_pid}, %{workers: workers, monitors: monitors} = state) do
