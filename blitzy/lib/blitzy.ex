6cc8c607c8b44466f77c2d4cd4675b0c0e8799f2
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2022-10-01 19:54:48 +0200

    blitzy: Adding parse_results

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -5,5 +5,54 @@ defmodule Blitzy do
     1..num_workers
     |> Enum.map(fn _ -> Task.async(worker_fun) end)
     |> Enum.map(&Task.await(&1, :infinity))
+    |> Enum.map(fn {_pid, result} -> result end)
+    |> parse_results()
+  end
+
+  defp parse_results(results) do
+    # first get results (is a list) and split it into
+    # success and failures knowing that {:ok, anything} is
+    # a success and anything else a failure
+    {successes, _failures} =
+      results
+      |> Enum.split_with(fn r ->
+        case r do
+          {:ok, _} -> true
+          _ -> false
+        end
+      end)
+
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failures = total_workers - total_success
+
+    data =
+      successes
+      |> Enum.map(fn {:ok, time} -> time end)
+
+    average_time = average(data)
+    longest_time = Enum.max(data)
+    shortest_time = Enum.min(data)
+
+    IO.puts("""
+    ----------
+    Total Workers     :   #{total_workers}
+    Successful reqs   :   #{total_success}
+    Failed reqs       :   #{total_failures}
+    Average(ms)       :   #{average_time}
+    Longest(ms)       :   #{longest_time}
+    Shortest(ms)      :   #{shortest_time}
+    ----------
+    """)
+  end
+
+  defp average(list) do
+    sum = Enum.sum(list)
+
+    if sum > 0 do
+      sum / Enum.count(list)
+    else
+      0
+    end
   end
 end
