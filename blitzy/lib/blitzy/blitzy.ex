438ba8ff4957fa59d101f4cc2451ae03fee68aa5
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2022-10-01 19:28:39 +0200

    blitzy: Running workers using Task

---
+++ blitzy/lib/blitzy/blitzy.ex
@@ -0,0 +1,9 @@
+defmodule Blitzy.Blitzy do
+  def run(num_workers, url) when num_workers > 0 do
+    worker_fun = fn -> Blitzy.Worker.start(url, self()) end
+
+    1..num_workers
+    |> Enum.map(fn _ -> Task.async(worker_fun) end)
+    |> Enum.map(&Task.await(&1))
+  end
+end
