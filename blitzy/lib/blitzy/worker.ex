0411bdb2dfe5546978c88de1c272c0a147720702
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2022-10-01 19:01:26 +0200

    blitzy: New simple worker

---
+++ blitzy/lib/blitzy/worker.ex
@@ -0,0 +1,32 @@
+defmodule Blitzy.Worker do
+  use Timex
+
+  require Logger
+
+  def start(url) do
+    # Duration.measure returns a tuple with the elapsedtime
+    # for the executed function and the function return value
+    {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
+
+    handle_response({Duration.to_milliseconds(timestamp), response})
+  end
+
+  defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
+       when code >= 200 and code <= 304 do
+    Logger.info("worker [#{node()}-#{inspect(self())}] completed in #{msecs} ms")
+
+    {:ok, msecs}
+  end
+
+  defp handle_response({_msecs, {:error, reason}}) do
+    Logger.info("worker [#{node()}-#{inspect(self())}] error due to #{inspect(reason)}")
+
+    {:error, reason}
+  end
+
+  defp handle_response({_msecs, _}) do
+    Logger.info("worker [#{node()}-#{inspect(self())}] unknown error")
+
+    {:error, :unknown}
+  end
+end
