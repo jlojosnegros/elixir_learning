bf7fad347b93d2e159e4093451fb8a19bd5ff7a9
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2023-08-18 11:38:42 +0200

    hangman: html_client: Modify router to handle hangman
    We are going to use /hangman as the base path for the game.
    This way we could use the same web server to handle multiple apps
    in parallel using different paths.
    
    Note: Using phoenix 1.17 changes all the folder layout from previous
         versions, nothing too hard right now, but maybe would have to
         come back in the near future and redo this ...

--- hangman/b1/lib/b1_web/controllers/hangman_controller.ex
+++ hangman/b1/lib/b1_web/controllers/hangman_controller.ex
@@ -1,9 +1,9 @@
-defmodule B1Web.PageController do
+defmodule B1Web.HangmanController do
   use B1Web, :controller
 
-  def home(conn, _params) do
+  def index(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :index, layout: false)
   end
 end
