f90273d356c4a48ae7ceb6a2f37ff9a64fd3225d
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2023-08-22 23:34:08 +0200

    hangman: html_client: Create new form
    Create a form using "form_for" and handle the POST
    with the new :update method in the HangmanController
    
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- hangman/b1/lib/b1_web/controllers/hangman_controller.ex
+++ hangman/b1/lib/b1_web/controllers/hangman_controller.ex
@@ -1,5 +1,4 @@
 defmodule B1Web.HangmanController do
-  alias Phoenix.Router
   use B1Web, :controller
 
   def index(conn, _params) do
@@ -38,6 +37,27 @@ defmodule B1Web.HangmanController do
     # view ( and template ) under the name "tally"
     # note: in the template we can refer to it inside the
     # elixir code blocks (<% %>) as "@tally"
+    render(conn, "game.html", tally: tally)
+  end
+
+  def update(conn, params) do
+    # We can get the game from the session as we have
+    # stored it earlier in the new method.
+    game = get_session(conn, :game)
+
+    # The guess comes in the params from the form.
+    # In the template we have instructed Elixir to
+    # save the form data into an structure(map) call
+    # "make_move", and the "guess" letter in a field
+    # called "guess" ... so here we can read it.
+    guess = params["make_move"]["guess"]
+
+    # Then we just need to call the Hangman server
+    # to make the move with the readed data.
+    tally = Hangman.make_move(game,guess)
+
+    # And render again the same template with the
+    # new tally information returned by server
     render(conn, "game.html", tally: tally)
   end
 end
