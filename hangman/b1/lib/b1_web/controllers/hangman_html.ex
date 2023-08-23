bdb8e58b7f12ee0e65c3d07879c83ee12b481ec4
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2023-08-24 00:00:41 +0200

    hangman: html_client: HTML layout improvements?
    Define some helper functions to render the template easely.
    Also some CSS to change a little bit the html ... although
    something does not work properly with that
    
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- hangman/b1/lib/b1_web/controllers/hangman_html.ex
+++ hangman/b1/lib/b1_web/controllers/hangman_html.ex
@@ -101,4 +101,32 @@ defmodule B1Web.HangmanHTML do
     =========
     """
   end
+
+  @status_fields %{
+    initializing: {"initializing", "guess the word, a letter at a time"},
+    good_guess: {"good-guess", "Good guess"},
+    bad_guess: {"bad-guess", "Sorry, that is a bad guess"},
+    won: {"won", "You won!"},
+    lost: {"lost", "Sorry you lost!"},
+    already_used: {"already-used", "You already used that letter"}
+  }
+  def move_status(status) do
+    {class, message} = @status_fields[status]
+    ~s(<div class="status #{class}">#{message}</div>)
+  end
+
+  def continue_or_try_again(_conn, status) when status in [:won, :lost] do
+    button("Try again", to: ~p"/hangman")
+  end
+
+  def continue_or_try_again(conn, _status) do
+    form_for(
+      conn,
+      ~p"/hangman",
+      [as: "make_move", method: :put],
+      fn f ->
+        [text_input(f, :guess), " ", submit("Make next guess")]
+      end
+    )
+  end
 end
