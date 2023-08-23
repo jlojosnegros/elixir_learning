3a86b71c092291ac8d4aee026a986bb94a60cbdb
Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>
2023-08-23 23:26:07 +0200

    hangman: html_client: Improve(?) html layout.
    Trying to make it a little bit less awful... but it seems
    like if CSS has abandoned this project ...
    
    Signed-off-by: Jose Luis Ojosnegros Manchón <jl.ojosnegros.manchon@gmail.com>

--- hangman/b1/lib/b1_web/controllers/hangman_html.ex
+++ hangman/b1/lib/b1_web/controllers/hangman_html.ex
@@ -5,4 +5,100 @@ defmodule B1Web.HangmanHTML do
   use Phoenix.HTML
 
   embed_templates "hangman_html/*"
+
+  def figure_for(0) do
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     / \\  |
+          |
+    =========
+    """
+  end
+
+  def figure_for(1) do
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+     /    |
+          |
+    =========
+    """
+  end
+
+  def figure_for(2) do
+    """
+      +---+
+      |   |
+      O   |
+     /|\\  |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(3) do
+    """
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(4) do
+    """
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+
+    =========
+    """
+  end
+
+  def figure_for(5) do
+    """
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(6) do
+    """
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+    """
+  end
+
+  def figure_for(7) do
+    """
+      +---+
+          |
+          |
+          |
+          |
+          |
+    =========
+    """
+  end
 end
